e13684e487c39100835b6555048c4e2c509df8f2
pchellovod <pchellovod@gmail.com>
2016-12-23 00:47:50 +0500

    add route and some links

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,7 @@
 class Route < ApplicationRecord
   validates :name, presence: true
+  has_many :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
+
+  #has_and_belongs_to_many :railway_stations
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,3 +1,5 @@
 class Train < ApplicationRecord
-  validates :title, presence: true
+  validates :number, presence: true
+
+  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
 end
