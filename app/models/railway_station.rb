b2bfa0ad5dcc9ba6617dbf57a8af2e696d2f52d4
pchellovod <pchellovod@gmail.com>
2016-12-28 01:31:32 +0500

    lesson 13 - some errors

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,6 +1,6 @@
 class RailwayStation < ApplicationRecord
   #has_and_belongs_to_many :routes
-  has_many :trains
+  has_many :trains, foreign_key: :current_station_id
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
