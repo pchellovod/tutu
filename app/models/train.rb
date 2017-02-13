9a5297ae121b31f7507d43e040be5178663af9a6
pchellovod <pchellovod@gmail.com>
2017-02-13 12:17:25 +0500

    repair carriages

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,16 +2,16 @@ class Train < ApplicationRecord
   belongs_to :route
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   has_many :tickets
-  has_many :carriages, before_add: :add_number
+  has_many :carriages
 
   validates :number, presence: true
 
-  def counting_seats(type_carriage, type_seat)
-    carriage.where('type = ?', type_carriage).sum("#{type_seat}".to_sym)
+  def count_seats_by_type(type_carriage, type_seat)
+    carriages.where(type: type_carriage).sum(type_seat)
   end
 
-  def current_order_carriages
-    order_carriages ? carriages.head : carriages.tail
+  def count_carriages_by_type(type_carriage)
+    carriages.where(type: type_carriage).count
   end
 
 end
