40629b6cf9c56a1549dc0d6d9465e2bbe6f676f2
pchellovod <pchellovod@gmail.com>
2017-02-18 23:28:40 +0500

    fix carriage names

--- app/assets/javascripts/carriages.js
+++ app/assets/javascripts/carriages.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
         carriage_type = $(this).val();
 
         var seats = {};
-        seats['BusinessCarriage'] = ['field_lower_seats'];
+        seats['LuxCarriage'] = ['field_bottom_seats'];
         seats['SittingCarriage'] = ['field_sitting_seats'];
-        seats['CoupeCarriage'] = ['field_lower_seats', 'field_upper_seats'];
+        seats['CoupeCarriage'] = ['field_bottom_seats', 'field_top_seats'];
         seats['EconomyCarriage'] = [
-            'field_lower_seats',
-            'field_upper_seats',
-            'field_side_upper_seats',
-            'field_side_lower_seats'
+            'field_bottom_seats',
+            'field_top_seats',
+            'field_side_top_seats',
+            'field_side_bottom_seats'
         ];
         seats['all'] = seats['EconomyCarriage'].concat(seats['SittingCarriage']);
 
