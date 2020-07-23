b514a559d163baa6d08b39884152836a9a1912b4
Dmitry <pchellovod@gmail.com>
2020-07-23 16:57:29 +0500

    fix duplicate migration

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'twitter-bootstrap-rails'
 gem 'rails_12factor'
 gem 'postmark-rails', '>=0.10.0'
 
+gem 'pry-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,92 +10,92 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208195506) do
+ActiveRecord::Schema.define(version: 2017_02_08_195506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "carriages", force: :cascade do |t|
-    t.string   "number"
-    t.integer  "bottom_seats"
-    t.integer  "top_seats"
-    t.integer  "train_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "type"
-    t.integer  "side_top_seats"
-    t.integer  "side_bottom_seats"
-    t.integer  "sitting_seats"
-    t.index ["id", "type"], name: "index_carriages_on_id_and_type", using: :btree
-    t.index ["train_id"], name: "index_carriages_on_train_id", using: :btree
+  create_table "carriages", id: :serial, force: :cascade do |t|
+    t.string "number"
+    t.integer "bottom_seats"
+    t.integer "top_seats"
+    t.integer "train_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "type"
+    t.integer "side_top_seats"
+    t.integer "side_bottom_seats"
+    t.integer "sitting_seats"
+    t.index ["id", "type"], name: "index_carriages_on_id_and_type"
+    t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
-  create_table "railway_stations", force: :cascade do |t|
-    t.string   "title"
+  create_table "railway_stations", id: :serial, force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "railway_stations_routes", force: :cascade do |t|
+  create_table "railway_stations_routes", id: :serial, force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
     t.integer "position"
-    t.string  "arrival"
-    t.string  "departure"
+    t.string "arrival"
+    t.string "departure"
   end
 
-  create_table "routes", force: :cascade do |t|
-    t.string   "name"
+  create_table "routes", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "tickets", force: :cascade do |t|
-    t.string  "number"
+  create_table "tickets", id: :serial, force: :cascade do |t|
+    t.string "number"
     t.integer "user_id"
     t.integer "train_id"
     t.integer "start_station_id"
     t.integer "end_station_id"
-    t.string  "passenger_name"
-    t.string  "passenger_passport"
-    t.index ["end_station_id"], name: "index_tickets_on_end_station_id", using: :btree
-    t.index ["start_station_id"], name: "index_tickets_on_start_station_id", using: :btree
-    t.index ["train_id"], name: "index_tickets_on_train_id", using: :btree
-    t.index ["user_id"], name: "index_tickets_on_user_id", using: :btree
+    t.string "passenger_name"
+    t.string "passenger_passport"
+    t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
+    t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
+    t.index ["train_id"], name: "index_tickets_on_train_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
-  create_table "trains", force: :cascade do |t|
-    t.string   "number"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.integer  "current_station_id"
-    t.integer  "route_id"
-    t.boolean  "order_carriages"
-    t.index ["current_station_id"], name: "index_trains_on_current_station_id", using: :btree
-    t.index ["route_id"], name: "index_trains_on_route_id", using: :btree
+  create_table "trains", id: :serial, force: :cascade do |t|
+    t.string "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "current_station_id"
+    t.integer "route_id"
+    t.boolean "order_carriages", default: true
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.boolean  "admin",                  default: false
-    t.string   "last_name"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string "unconfirmed_email"
+    t.boolean "admin", default: false
+    t.string "last_name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
