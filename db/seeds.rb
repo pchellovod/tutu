c7cd950b527e09ab6037fe658773c5cbb034e355
Dmitry <dvoryankindm@gmail.com>
2020-07-31 17:08:31 +0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-rs1 = RailwayStation.first(id:1, title: 'Omsk')
-rs2 = RailwayStation.new(id: 2, title: 'Moscow')
-t1 = Train.new(number: '123')
-r1 = Route.new(name: 'Omsk - Moscow')
-r1.railway_stations << rs1
-r1.railway_stations << rs2
-t1.current_station << rs1
+# rs1 = RailwayStation.new(id:1, title: 'Omsk')
+# rs2 = RailwayStation.new(id: 2, title: 'Moscow')
+# t1 = Train.new(number: '123')
+# r1 = Route.new(name: 'Omsk - Moscow')
+# r1.railway_stations << rs1
+# r1.railway_stations << rs2
+# t1.current_station << rs1
+
+10.times do
+  RailwayStation.create(
+      title: Faker::Address.city
+  )
+end
+
+10.times do
+  Route.create(
+      name: "#{Faker::Address.city} - #{Faker::Address.city}"
+  )
+end
 
 
+10.times do
+  Train.create(
+      number: Faker::Address.building_number
+  )
+end
